a4f989b04bc713391c29648a0a2bf4c721b8f0d3
Hugo Puhlmann <hugopuhlmann@gmail.com>
2013-01-06 19:42:20 -0200

    Adicionei tabela de preços na página do produto.

--- src/main/java/br/ufsm/inf/bolicho/payments/PaymentsTable.java
+++ src/main/java/br/ufsm/inf/bolicho/payments/PaymentsTable.java
@@ -2,6 +2,7 @@ package br.ufsm.inf.bolicho.payments;
 
 import br.ufsm.inf.bolicho.beans.Product;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,25 +14,26 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class PaymentsTable {
-    public static final double MIN_INTEREST = 1.08;
-    public static final double MONTHLY_INTEREST = 1.03; //this will be applied over base interest
+    public static final double MIN_INTEREST = 1.03;
+    public static final double MONTHLY_INTEREST = 1.005; //this will be applied over base interest
     public static final int MIN_PAYMENTS_FOR_INTEREST = 6;
 
-    public List<String> getPaymentsTable(Product product) {
+    public static List<String> getPaymentsTable(Product product) {
         int maxPayments = getMaxPayments(product.getPrice());
         List<String> payments = new ArrayList<String>();
         StringBuilder builder = new StringBuilder();
-        for (int i = 1; i < maxPayments; ++i) {
+        DecimalFormat decimalFormat = new DecimalFormat("0.000");
+        for (int i = 1; i <= maxPayments; ++i) {
             builder.append(i);
             builder.append("x de ");
             if (i > MIN_PAYMENTS_FOR_INTEREST) {
-                double accumulativeInterest = 1 - Math.pow(MONTHLY_INTEREST, i - MIN_PAYMENTS_FOR_INTEREST);
+                double accumulativeInterest = Math.pow(MONTHLY_INTEREST, i - MIN_PAYMENTS_FOR_INTEREST) - 1;
                 double monthlyInterest = MIN_INTEREST + accumulativeInterest;
                 double interest = Math.pow(monthlyInterest, (i + 1) - MIN_PAYMENTS_FOR_INTEREST);
                 double finalAmount = product.getPrice()* interest;
                 builder.append(finalAmount/i);
                 builder.append(" R$ (");
-                builder.append((1 - monthlyInterest)*100);
+                builder.append(decimalFormat.format((monthlyInterest - 1)*100));
                 builder.append("% a.m.)");
             } else {
                 builder.append(product.getPrice()/i);

24f11273d37d7290b8934a879455f01184bd0043
Hugo Puhlmann <hugopuhlmann@gmail.com>
2013-02-17 00:01:13 -0300

    Trabalho inicial no NewOrderDAO.

---
+++ src/main/java/br/ufsm/inf/bolicho/dao/NewOrderDAO.java
@@ -0,0 +1,11 @@
+package br.ufsm.inf.bolicho.dao;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hstefan
+ * Date: 16/02/13
+ * Time: 23:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class NewOrderDAO {
+}

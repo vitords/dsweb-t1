aa74fc3e971a089b8188833013cf3458fac7449f
Hugo Puhlmann <hugopuhlmann@gmail.com>
2013-02-17 00:22:26 -0300

    Criei entidade para Orders.

--- src/main/java/br/ufsm/inf/bolicho/model/Order.java
+++ src/main/java/br/ufsm/inf/bolicho/model/Order.java
@@ -1,5 +1,6 @@
 package br.ufsm.inf.bolicho.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
@@ -10,11 +11,22 @@ import java.util.List;
  * Time: 23:10
  * To change this template use File | Settings | File Templates.
  */
+@Entity
+@Table(name = "\"Order\"")
 public class Order implements Serializable {
-
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator =  "order_id_seq")
+    @SequenceGenerator(name = "order_id_gen", sequenceName = "order_id_seq", allocationSize = 1)
     private int id;
+    @OneToOne
+    @Column(name = "user")
     private User user;
+    @OneToMany
+    @Column(name = "products")
     private List<Product> products;
+    @OneToMany
+    @Column(name = "selected_products")
     private Product[] selectedProducts;
 
     public int getId() {

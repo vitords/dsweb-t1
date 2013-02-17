18f1533715cc31bc26bce72c908dd1b2bb51aa2a
Hugo Puhlmann <hugopuhlmann@gmail.com>
2013-02-17 00:21:21 -0300

    Tinham ficado algumas definições de fora do commit anterior.

--- src/main/java/br/ufsm/inf/bolicho/dao/NewOrderDAO.java
+++ src/main/java/br/ufsm/inf/bolicho/dao/NewOrderDAO.java
@@ -1,4 +1,5 @@
 package br.ufsm.inf.bolicho.dao;
+import br.ufsm.inf.bolicho.model.Order;
 
 /**
  * Created with IntelliJ IDEA.
@@ -7,5 +8,17 @@ package br.ufsm.inf.bolicho.dao;
  * Time: 23:56
  * To change this template use File | Settings | File Templates.
  */
-public class NewOrderDAO {
+public class NewOrderDAO extends NewGenericDAO<Order> {
+    public void salvar(Order order) {
+        save(order);
+    }
+
+    public void alterar(Order order) {
+        update(order);
+    }
+
+    public void excluir(int id) {
+        Order o = findById(id);
+        delete(o);
+    }
 }

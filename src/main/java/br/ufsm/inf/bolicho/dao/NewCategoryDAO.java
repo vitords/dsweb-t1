975487386c3da6103c9f78d115d542f664e8f597
Leonardo Quatrin Campagnolo <lennom@gmail.com>
2013-02-17 11:44:28 -0300

    Apenas adicionando o NewCategoryDAO que tinha faltado

---
+++ src/main/java/br/ufsm/inf/bolicho/dao/NewCategoryDAO.java
@@ -0,0 +1,25 @@
+package br.ufsm.inf.bolicho.dao;
+
+import br.ufsm.inf.bolicho.model.Category;
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vitor
+ * Date: 04/01/13
+ * Time: 23:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class NewCategoryDAO extends NewGenericDAO<Category> {
+
+    public void salvar(Category category) {
+        save(category);
+    }
+
+    public void alterar(Category category) {
+        update(category);
+    }
+
+    public void excluir(int id) {
+        Category o = findById(id);
+        delete(o);
+    }
+}
